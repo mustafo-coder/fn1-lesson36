6bd77405ee1d0f2021f2abda8870790a863e083e
ogabek <ogbkiy@gmail.com>
2025-01-15 10:42:07 +0500

    results done

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
+import Category from '@/components/Category'
 import DreamCompany from '@/components/DreamCompany'
 import RecommendeCompany from '@/components/RecommendeCompany'
+import Results from '@/components/Results'
 import React from 'react'
 
 export default function page() {
@@ -7,6 +9,8 @@ export default function page() {
     <div>
       <DreamCompany/>
       <RecommendeCompany/>
+      <Category/>
+      <Results/>
     </div>
   )
 }
--- src/images/index.js
+++ src/images/index.js
@@ -9,3 +9,22 @@ export { default as Udacity } from "./udacity.svg";
 export { default as Webflow } from "./Webflow.svg";
 export { default as Foundation } from "./foundation.svg";
 
+export { default as Design } from "./design.svg";
+export { default as Fintech } from "./fintech.svg";
+export { default as Hosting } from "./hosting.svg";
+export { default as BusinessService } from "./businnes_service.svg";
+export { default as Development } from "./development.svg";
+
+export { default as Pentagram } from "./pentagram.svg";
+export { default as WolffOlins } from "./wolff-olins.svg";
+export { default as Clay } from "./clay.svg";
+export { default as MediaMonks } from "./media-monks.svg";
+export { default as Packer } from "./packer.svg";
+export { default as Square } from "./square.svg";
+export { default as Divy } from "./divy.svg";
+
+export { default as Row } from "./row.svg";
+
+
+
+
