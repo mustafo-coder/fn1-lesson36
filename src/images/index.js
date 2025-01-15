f84c9bd0892a021cadab258a298b003e01e5bf75
Nurmurod01 <nurmurodrabbimov@gmail.com>
2025-01-15 16:22:59 +0500

    salom

--- src/images/index.js
+++ src/images/index.js
@@ -1,1 +1,4 @@
 export { default as Revolut } from "./Revolut.svg";
+export { default as AvatarIcon } from "./avatar-icon.png";
+export { default as Alex } from "./Alex-defoult-avatar.png";
+export { default as Textfield } from "./Click-to-replace.png";
