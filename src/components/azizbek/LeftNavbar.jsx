8e55008c4f4a8dac352863db4939c254c4483e1a
azizbeknoder <azizbektuychiyev4663@gmail.com>
2025-01-15 16:04:44 +0500

    dashboard window bitdi

---
+++ src/components/azizbek/LeftNavbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LeftNavbar() {
+  return (
+    <div className='w-[272px] h-[962px] bg-green-500 '>
+            Left Navbar
+    </div>
+  )
+}
---
+++ src/components/azizbek/TopNavbar.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TopNavbar() {
+  return (
+    <div className='h-[114px] bg-yellow-400 w-full'>TopNavbar</div>
+  )
+}
