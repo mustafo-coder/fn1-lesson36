c56e93643838ba58844cdbb1a02e26b39c2304f5
bekzod1337 <rahimjonovazarina@gmail.com>
2025-01-15 16:23:15 +0500

    bekzod

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,7 +18,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" theme='light'>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
